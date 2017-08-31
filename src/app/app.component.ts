9b55ae60eeee9916a4fdf9e38f313c7e51017d49
= <=>
2017-08-31 15:16:00 +0100

    final commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,23 +21,31 @@ showVar: boolean = true;
 calculateModel = function() {
 
   this.myNum = parseInt(this.number);
-  if (this.myNum>1) {
+try {
+  if(this.myNum<2) throw "Enter a number greater than 1";
+  if(isNaN(this.myNum)) throw "Value given is not a number";
+
+// executes mathematics models derived
   this.no_of_cubelets = Math.pow(this.myNum, 3);
   this.external_cubelets = (6 * Math.pow(this.myNum-1, 2)) + 2;
   this.internal_cubelets = Math.pow(this.myNum-2, 3);
 
 //this iterates through n to find a subset of n
+if (this.myNum<=3){
+  this.derived_rubix = 0;
+} else {
   for (var i = this.myNum-1; i>1; i--){
     this.derived_cube = Math.pow(i, 3);
       if (this.derived_cube === this.internal_cubelets){
         this.derived_rubix = i;
          break;
        }
+     }
   }
-} else {
-  alert("Please enter a value of n greater than 1");
-  this.number.value="";
 }
-}
-
+catch(err){
+  alert(err);
+  this.number = "";
+    }
+  }
 }
