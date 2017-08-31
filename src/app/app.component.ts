0c4a2306e918dde895ddbbcabba9edb1771647c3
= <=>
2017-08-31 10:14:40 +0100

    Final Stage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,16 +6,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-//volume;
+
 number;
+myNum = 0;
+no_of_cubelets;
+external_cubelets;
+internal_cubelets;
+derived_cube;
+derived_rubix;
+showVar: boolean = true;
 calculateModel = function() {
-  var myNum = parseInt(this.number);
-  var no_of_cubelets = Math.pow(myNum, 3);
-  var total_cubelets = (6 * Math.pow(myNum-1, 2)) + 2;
-  var hidden_cubelets = Math.pow(myNum-2, 3);
-  
-  console.log(no_of_cubelets);
-  console.log(total_cubelets);
-  console.log(hidden_cubelets);
+
+  this.myNum = parseInt(this.number);
+  this.no_of_cubelets = Math.pow(this.myNum, 3);
+  this.external_cubelets = (6 * Math.pow(this.myNum-1, 2)) + 2;
+  this.internal_cubelets = Math.pow(this.myNum-2, 3);
+
+  for (var i = this.myNum-1; i>1; i--){
+    this.derived_cube = Math.pow(i, 3);
+      if (this.derived_cube === this.internal_cubelets){
+        this.derived_rubix = i;
+        break;
+      }
+
+    this.showVar = !this.showVar;
+  }
+
+  // console.log("total number of cubelets in "+ this.myNum + " = " + this.no_of_cubelets);
+  // console.log("visible cube in "+ this.myNum + " = " + this.external_cubelets);
+  // console.log("hidden cube in "+ this.myNum + " = " + this.internal_cubelets);
 }
+
 }
